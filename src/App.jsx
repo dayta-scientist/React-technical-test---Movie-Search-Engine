620ae3890fc19902135966b7adcd0eeda83eae26
Gabriel LR <dayta.scientist@gmail.com>
2023-07-11 05:23:45 -0300

    First mapping in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import responseMovies from "./mocks/with-results.json"
 
 function App() {
+  const movies = responseMovies.Search
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body>
+        <header>
+          <h1>Movie search engine</h1>
+          <form className='form'>
+            <label htmlFor="search">Search a film</label>
+            <input type="text" placeholder='Avengers, Star Wars...' id='search' />
+            <button type='submit'>Search</button>
+          </form>
+        </header>
+
+        <main>
+          {movies ?
+            <ul>
+              {movies?.map(movie => {
+            return(
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            )
+          })}
+            </ul>
+          
+          : <p>No movies found</p>
+        }
+        </main>
+      </body>
     </>
   )
 }
